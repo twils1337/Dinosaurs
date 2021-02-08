4992726f98e2b6970d2893ab334f118c7ffc261e
Tyler Wilson <tylerwilson2323@gmail.com>
2021-02-08 03:43:44 -0800

    Feedback implemented
    -Added comments to app.js
    -Added info icon on the page to let the user know they can go back to input form if they click anywhere on the page.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-// Create Constructors
-
+/**
+* @description Constructor for parent class that both Dino & Human will inherit from
+* @constructor
+* @param {string} species - The type of animal
+* @param {number} weight - How much the animal weighs
+* @param {number} height - How tall the animal is
+* @param {string} diet - What the animal eats
+*/
 function BaseAnimal(species, weight, height, diet) {
   this.species = species;
   this.weight = weight;
@@ -7,6 +13,14 @@ function BaseAnimal(species, weight, height, diet) {
   this.diet = diet;
 }
 
+/**
+ * @description Constructor for Dino class
+ * @constructor 
+ * See BaseAnimal for description of params species through diet
+ * @param {string} where - Location where the Dino primarily lived
+ * @param {string} when - Time period when the Dino lived
+ * @param {string} fact - Random fact about the Dino
+ */
 function Dino(species, weight, height, diet, where, when, fact) {
   BaseAnimal.call(this, species, weight, height, diet);
   this.where = where;
@@ -14,12 +28,17 @@ function Dino(species, weight, height, diet, where, when, fact) {
   this.fact = fact;
 }
 
+/**
+ * @description Constructor for Human class
+ * @constructor
+ * See BaseAnimal for details about param's height through diet
+ */
 function Human(name, height, weight, diet) {
   BaseAnimal.call(this, 'Human', weight, height, diet);
   this.name = name;
 }
 
-// Create Dino Objects
+// Construction of all the Dino's that are to be used in UI
 const dinos = [
   new Dino(
     'Triceratops',
@@ -95,8 +114,7 @@ const dinos = [
   )
 ];
 
-// Create Human Object
-// Use IIFE to get human data from form
+// function to grab user input that will help generate the Human square in the display results
 const getHumanData = function() {
   return (function() {
     return new Human(
@@ -109,8 +127,8 @@ const getHumanData = function() {
   })();
 };
 
-// NOTE: Weight in JSON file is in lbs.
-// Create Dino Compare Method 1
+// Comparing functions that will help with the generation of random facts about each dinosaur
+// ===========================================================================================================
 function compareWeight(dino, human) {
   const heaviestAnimal = dino.weight > human.weight ? 'dinosaur' : 'human';
   const lightestAnimal = dino.weight > human.weight ? 'human' : 'dinosaur';
@@ -119,8 +137,7 @@ function compareWeight(dino, human) {
   )} more lbs. than this ${lightestAnimal}.`;
 }
 
-// Create Dino Compare Method 2
-// NOTE: Heightin JSON file is in inches.
+
 function compareHeight(dino, human) {
   const tallestAnimal = dino.height > human.height ? 'dinosaur' : 'human';
   const shortestAnimal = dino.height > human.height ? 'human' : 'dinosaur';
@@ -137,8 +154,9 @@ function compareDiet(dino, human) {
     return `This dinosar eats a ${dino.diet} diet and this human eats a ${human.diet} diet.`;
   }
 }
+// ============================================================================================================
 
-// Generate Tiles for each Dino in Array
+// Will generate the tiles in the result grid after the user enters information and hit's the Compare Me! button
 function generateTiles() {
   const human = getHumanData();
   const grid = document.getElementById('grid');
@@ -151,6 +169,8 @@ function generateTiles() {
   }
 }
 
+// Helper function to help generate all the html required to properly generate the
+// cells within the grid and properly display it.
 function generateCell(dino, human) {
   const dataSource = dino != null ? dino : human;
   const cell = document.createElement('div');
@@ -170,6 +190,9 @@ function generateCell(dino, human) {
   return cell;
 }
 
+// Helper function to help randomly generate a fact to display in the results grid. 
+// Potentially utilizes 1 of the 3 comparator functions or default fact in order to
+// generate. 
 function getFact(dino, human) {
   if (dino.species === 'Pigeon') {
     return dino.fact;
@@ -187,9 +210,9 @@ function getFact(dino, human) {
   }
 }
 
-// Add tiles to DOM
-// Remove form from screen
-// On button click, prepare and display infographic
+// Allows for the user to toggle between the input form and the result grid.
+// This allows the user to either change the info. of the human to see different
+// calculations compared to the dinosaur and/or see different facts about the dino.'s.
 // eslint-disable-next-line no-unused-vars
 function togglePage(showForm) {
   const form = document.getElementById('dino-compare');
